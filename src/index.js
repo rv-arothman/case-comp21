34d338d191f0d73aabc21d9d268c360cfcea55fb
Enoc <enoc.flores.ef@gmail.com>
2021-08-10 12:44:02 -0400

    Trying out a dropdown to fetch api with

--- src/index.js
+++ src/index.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class FetchAPI extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'title'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your choice was: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <select value={this.state.value} onChange={this.handleChange}>
+          <option value="title">Media</option>
+          <option value="movie">Movie</option>
+          <option value="show">Show</option>
+        </select>
+        <select value={this.state.value} onChange={this.handleChange}>
+          <option value="title">Genre</option>
+          <option value="horror">Horror</option>
+          <option value="action">Action</option>
+          <option value="thriller">Thriller</option>
+        </select>
+        <select value={this.state.value} onChange={this.handleChange}>
+          <option value="title">Stream</option>
+          <option value="netflix">Netflix</option>
+          <option value="hbo">HBO Max</option>
+          <option value="disney">Disney +</option>
+        </select>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <FetchAPI/>,
   document.getElementById('root')
 );
 
