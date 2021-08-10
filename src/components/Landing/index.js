adf9c845e7d3d7c50655261131e05095b50d472e
Eden Chou <eden.chou0606@gmail.com>
2021-08-10 12:35:48 -0400

    adding new options

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -10,7 +10,25 @@ function triggerGenre() {
     } else {
         document.getElementById("genre-dropdown").classList.add("is-active");
     }
-    
+
+}
+
+function triggerType() {
+    if (document.getElementById("type-dropdown").classList.contains("is-active")) {
+        document.getElementById("type-dropdown").classList.remove("is-active");
+    } else {
+        document.getElementById("type-dropdown").classList.add("is-active");
+    }
+
+}
+
+function triggerRating() {
+    if (document.getElementById("rating-dropdown").classList.contains("is-active")) {
+        document.getElementById("rating-dropdown").classList.remove("is-active");
+    } else {
+        document.getElementById("rating-dropdown").classList.add("is-active");
+    }
+
 }
 
 function triggerYear() {
@@ -29,6 +47,7 @@ function triggerStream() {
     }
 }
 
+
 function Landing() {
     return (
         <section className="main">
@@ -40,7 +59,50 @@ function Landing() {
                     <div className="dropdown-trigger">
                         <button className="button button-size" aria-haspopup="true" aria-controls="dropdown-menu">
                             <div className="button-text-container"><span className="main-button-name">GENRE</span>
-                                <span className="icon is-small">
+                                <span className="icon is-medium">
+                                    <FontAwesomeIcon className="button-arrow" icon={faAngleDown} />
+                                </span></div>
+                        </button>
+                    </div>
+                    <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                        <div className="dropdown-content">
+                            <a href="edenchou.com" className="dropdown-item">
+                                Dropdown item
+                            </a>
+                            <a href="edenchou.com" className="dropdown-item">
+                                Other dropdown item
+                            </a>
+                            <a href="edenchou.com" className="dropdown-item is-active">
+                                Active dropdown item
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div id="type-dropdown" className="dropdown" onClick={triggerType}>
+                    <div className="dropdown-trigger">
+                        <button className="button button-size" aria-haspopup="true" aria-controls="dropdown-menu">
+                            <div className="button-text-container"><span className="main-button-name">TYPE</span>
+                                <span className="icon is-medium">
+                                    <FontAwesomeIcon className="button-arrow" icon={faAngleDown} />
+                                </span></div>
+                        </button>
+                    </div>
+                    <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                        <div className="dropdown-content">
+                            <a href="edenchou.com" className="dropdown-item movie-option">
+                                MOVIE
+                            </a>
+                            <a href="edenchou.com" className="dropdown-item is-active">
+                                TV SHOW
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div id="rating-dropdown" className="dropdown" onClick={triggerRating}>
+                    <div className="dropdown-trigger">
+                        <button className="button button-size" aria-haspopup="true" aria-controls="dropdown-menu">
+                            <div className="button-text-container"><span className="main-button-name">RATING</span>
+                                <span className="icon is-medium">
                                     <FontAwesomeIcon className="button-arrow" icon={faAngleDown} />
                                 </span></div>
                         </button>
